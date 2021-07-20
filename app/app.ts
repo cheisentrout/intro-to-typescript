3ed3e8ac8bccb932c488be71fa29130b17b79b66
Clare Eisentrout <ceisentrout@predictiveindex.com>
2021-07-20 16:03:39 -0400

    Configuration for TS project

--- app/app.ts
+++ app/app.ts
@@ -1,7 +1,13 @@
 function startGame() {
   // starting a new game
   var messagesElement = document.getElementById('messages');
+  // non-null assertion operator
   messagesElement!.innerText = 'Welcome to MultiMath! Starting new game...';  
 }
 
 document.getElementById('startGame')!.addEventListener('click', startGame);
+
+/* Compiling this ts file into js:
+    In terminal, navigate to the root folder for this file ("app")
+    Run the command tsc app.ts
+*/
